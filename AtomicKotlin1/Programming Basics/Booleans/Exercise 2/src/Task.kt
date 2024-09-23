5dc70bdc1838f8efe43dd14af7e6b49ab1d82816
Alexey Gvozdarev <lelikfreeride@gmail.com>
2024-09-23 16:28:03 +0700

    Booleans course done

--- AtomicKotlin1/Programming Basics/Booleans/Exercise 2/src/Task.kt
+++ AtomicKotlin1/Programming Basics/Booleans/Exercise 2/src/Task.kt
@@ -1,12 +1,32 @@
 // Booleans/Task2.kt
 package booleansExercise2
 
-fun showAnd() {}
+fun showAnd(first: Boolean, second: Boolean) {
+  val result = first && second
+  println("$first && $second == $result")
 
-fun showOr() {}
+}
+
+fun showOr(first: Boolean, second: Boolean) {
+  val result = first || second
+  println("$first || $second == $result")
+}
 
 fun showTruthTable() {
-  TODO()
+  println("Truth Table for AND (&&):")
+  println("----------------------------")
+
+  showAnd(true, true)
+  showAnd(true, false)
+  showAnd(false, true)
+  showAnd(false, false)
+
+  println("\nTruth Table for OR (||):")
+  println("---------------------------")
+  showOr(true, true)
+  showOr(true, false)
+  showOr(false, true)
+  showOr(false, false)
 }
 
 fun main() {
--- AtomicKotlin1/Programming Basics/Booleans/Exercise 3/src/Task.kt
+++ AtomicKotlin1/Programming Basics/Booleans/Exercise 3/src/Task.kt
@@ -1,13 +1,19 @@
 // Booleans/Task3.kt
 package booleansExercise3
 
-fun and(b1: Boolean, b2: Boolean): Boolean =
-  TODO()
+fun and(b1: Boolean, b2: Boolean): Boolean {
+  if (b1 && b2) {
+    return true
+  }else return false
+}
+
 
 fun or(b1: Boolean, b2: Boolean): Boolean =
-  TODO()
+  if (b1) true else b2
+
+
 
 fun main() {
   println(and(true, true)) // true
-  println(or(true, false)) // true
+   println(or(true, false)) // true
 }
