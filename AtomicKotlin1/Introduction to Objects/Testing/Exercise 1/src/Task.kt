e21dc975d6e2b37f50bd4c392ff4cec329b559f9
Alexey Gvozdarev <lelikfreeride@gmail.com>
2024-11-17 18:56:44 +0700

    Testing is done

--- AtomicKotlin1/Introduction to Objects/Testing/Exercise 1/src/Task.kt
+++ AtomicKotlin1/Introduction to Objects/Testing/Exercise 1/src/Task.kt
@@ -1,9 +1,20 @@
 // Testing/Task1.kt
 package testingExercise1
 import atomictest.eq
+import kotlin.math.absoluteValue
 
-fun abs(number: Int): Int = TODO()
+fun abs(number: Int): Int {
+  if (number < 0){
+    val num = number.absoluteValue
+    return num
+  } else {
+    return number
+  }
+}
 
 fun main() {
-  TODO()
+  abs(0) eq 0
+  abs(42) eq 42
+  abs(-239) eq -239
+
 }
