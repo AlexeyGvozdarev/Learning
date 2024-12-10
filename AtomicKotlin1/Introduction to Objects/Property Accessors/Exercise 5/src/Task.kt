c3ec2628c43afaa96a928ca27aee8f74d9db0112
Alexey Gvozdarev <lelikfreeride@gmail.com>
2024-12-10 15:36:01 +0700

    Accessors is done

--- AtomicKotlin1/Introduction to Objects/Property Accessors/Exercise 5/src/Task.kt
+++ AtomicKotlin1/Introduction to Objects/Property Accessors/Exercise 5/src/Task.kt
@@ -8,8 +8,9 @@ class Cage(private val maxCapacity: Int) {
   private val hamsters =
     mutableListOf<Hamster>()
 
-  val capacity: Int
-    get() = maxCapacity - hamsters.size
+  fun getCapacity(): Int {
+    return maxCapacity - hamsters.size
+  }
 
   val full: Boolean
     get() = hamsters.size == maxCapacity
@@ -29,12 +30,12 @@ class Cage(private val maxCapacity: Int) {
 fun main() {
   val cage = Cage(maxCapacity = 2)
   cage.full eq false
-  cage.capacity eq 2
+  cage.getCapacity() eq 2
   cage.put(Hamster("Alice")) eq true
   cage.put(Hamster("Bob")) eq true
   cage.full eq true
-  cage.capacity eq 0
+  cage.getCapacity() eq 0
   cage.put(Hamster("Charlie")) eq false
   cage.takeHamster()
-  cage.capacity eq 1
+  cage.getCapacity() eq 1
 }
